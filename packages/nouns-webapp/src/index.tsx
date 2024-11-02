abda3c8064745f37d9b0cce6cec95ba5429bb938
Godwin <godwinaquinas@gmail.com>
2024-11-02 10:30:41 +0100

    fix

--- packages/nouns-webapp/src/index.tsx
+++ packages/nouns-webapp/src/index.tsx
@@ -35,7 +35,7 @@ import config, {
 } from './config';
 import { WebSocketProvider } from '@ethersproject/providers';
 import { BigNumber, BigNumberish, Event } from 'ethers';
-import { NounsAuctionHouseFactory } from '@nouns/sdk';
+// import { NounsAuctionHouseFactory } from '@nouns/sdk';
 import dotenv from 'dotenv';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { appendBid } from './state/slices/auction';
@@ -129,86 +129,87 @@ const BLOCKS_PER_DAY = 7_200;
 const ChainSubscriber: React.FC = () => {
   const dispatch = useAppDispatch();
 
-  const loadState = async () => {
-    const wsProvider = new WebSocketProvider(config.app.wsRpcUri);
-    const nounsAuctionHouseContract = NounsAuctionHouseFactory.connect(
-      config.addresses.nounsAuctionHouseProxy,
-      wsProvider,
-    );
+  // const loadState = async () => {
+  //   const wsProvider = new WebSocketProvider(config.app.wsRpcUri);
+  //   const nounsAuctionHouseContract = NounsAuctionHouseFactory.connect(
+  //     config.addresses.nounsAuctionHouseProxy,
+  //     wsProvider,
+  //   );
 
-    const bidFilter = nounsAuctionHouseContract.filters.AuctionBid(null, null, null, null);
-    const extendedFilter = nounsAuctionHouseContract.filters.AuctionExtended(null, null);
-    const createdFilter = nounsAuctionHouseContract.filters.AuctionCreated(null, null, null);
-    const settledFilter = nounsAuctionHouseContract.filters.AuctionSettled(null, null, null);
-    const processBidFilter = async (
-      nounId: BigNumberish,
-      sender: string,
-      value: BigNumberish,
-      extended: boolean,
-      event: Event,
-    ) => {
-      const timestamp = (await event.getBlock()).timestamp;
-      const { transactionHash, transactionIndex } = event;
-      dispatch(
-        appendBid(
-          reduxSafeBid({
-            nounId,
-            sender,
-            value,
-            extended,
-            transactionHash,
-            transactionIndex,
-            timestamp,
-          }),
-        ),
-      );
-    };
-    const processAuctionCreated = (
-      nounId: BigNumberish,
-      startTime: BigNumberish,
-      endTime: BigNumberish,
-    ) => {
-      dispatch(
-        setActiveAuction(reduxSafeNewAuction({ nounId, startTime, endTime, settled: false })),
-      );
-      const nounIdNumber = BigNumber.from(nounId).toNumber();
-      dispatch(push(nounPath(nounIdNumber)));
-      dispatch(setOnDisplayAuctionNounId(nounIdNumber));
-      dispatch(setLastAuctionNounId(nounIdNumber));
-    };
-    const processAuctionExtended = (nounId: BigNumberish, endTime: BigNumberish) => {
-      dispatch(setAuctionExtended({ nounId, endTime }));
-    };
-    const processAuctionSettled = (nounId: BigNumberish, winner: string, amount: BigNumberish) => {
-      dispatch(setAuctionSettled({ nounId, amount, winner }));
-    };
+  //   const bidFilter = nounsAuctionHouseContract.filters.AuctionBid(null, null, null, null);
+  //   const extendedFilter = nounsAuctionHouseContract.filters.AuctionExtended(null, null);
+  //   const createdFilter = nounsAuctionHouseContract.filters.AuctionCreated(null, null, null);
+  //   const settledFilter = nounsAuctionHouseContract.filters.AuctionSettled(null, null, null);
+  //   const processBidFilter = async (
+  //     nounId: BigNumberish,
+  //     sender: string,
+  //     value: BigNumberish,
+  //     extended: boolean,
+  //     event: Event,
+  //   ) => {
+  //     const timestamp = (await event.getBlock()).timestamp;
+  //     const { transactionHash, transactionIndex } = event;
+  //     dispatch(
+  //       appendBid(
+  //         reduxSafeBid({
+  //           nounId,
+  //           sender,
+  //           value,
+  //           extended,
+  //           transactionHash,
+  //           transactionIndex,
+  //           timestamp,
+  //         }),
+  //       ),
+  //     );
+  //   };
+  //   const processAuctionCreated = (
+  //     nounId: BigNumberish,
+  //     startTime: BigNumberish,
+  //     endTime: BigNumberish,
+  //   ) => {
+  //     dispatch(
+  //       setActiveAuction(reduxSafeNewAuction({ nounId, startTime, endTime, settled: false })),
+  //     );
+  //     const nounIdNumber = BigNumber.from(nounId).toNumber();
+  //     dispatch(push(nounPath(nounIdNumber)));
+  //     dispatch(setOnDisplayAuctionNounId(nounIdNumber));
+  //     dispatch(setLastAuctionNounId(nounIdNumber));
+  //   };
+  //   const processAuctionExtended = (nounId: BigNumberish, endTime: BigNumberish) => {
+  //     dispatch(setAuctionExtended({ nounId, endTime }));
+  //   };
+  //   const processAuctionSettled = (nounId: BigNumberish, winner: string, amount: BigNumberish) => {
+  //     dispatch(setAuctionSettled({ nounId, amount, winner }));
+  //   };
 
-    // Fetch the current auction
-    const currentAuction = await nounsAuctionHouseContract.auction();
-    dispatch(setFullAuction(reduxSafeAuction(currentAuction)));
-    dispatch(setLastAuctionNounId(currentAuction.nounId.toNumber()));
+  //   // Fetch the current auction
+  //   const currentAuction = await nounsAuctionHouseContract.auction();
+  //   dispatch(setFullAuction(reduxSafeAuction(currentAuction)));
+  //   dispatch(setLastAuctionNounId(currentAuction.nounId.toNumber()));
 
-    // Fetch the previous 24 hours of bids
-    const previousBids = await nounsAuctionHouseContract.queryFilter(bidFilter, 0 - BLOCKS_PER_DAY);
-    for (let event of previousBids) {
-      if (event.args === undefined) return;
-      processBidFilter(...(event.args as [BigNumber, string, BigNumber, boolean]), event);
-    }
+  //   // Fetch the previous 24 hours of bids
+  //   const previousBids = await nounsAuctionHouseContract.queryFilter(bidFilter, 0 - BLOCKS_PER_DAY);
+  //   for (let event of previousBids) {
+  //     if (event.args === undefined) return;
+  //     processBidFilter(...(event.args as [BigNumber, string, BigNumber, boolean]), event);
+  //   }
 
-    nounsAuctionHouseContract.on(bidFilter, (nounId, sender, value, extended, event) =>
-      processBidFilter(nounId, sender, value, extended, event),
-    );
-    nounsAuctionHouseContract.on(createdFilter, (nounId, startTime, endTime) =>
-      processAuctionCreated(nounId, startTime, endTime),
-    );
-    nounsAuctionHouseContract.on(extendedFilter, (nounId, endTime) =>
-      processAuctionExtended(nounId, endTime),
-    );
-    nounsAuctionHouseContract.on(settledFilter, (nounId, winner, amount) =>
-      processAuctionSettled(nounId, winner, amount),
-    );
-  };
-  loadState();
+  //   nounsAuctionHouseContract.on(bidFilter, (nounId, sender, value, extended, event) =>
+  //     processBidFilter(nounId, sender, value, extended, event),
+  //   );
+  //   nounsAuctionHouseContract.on(createdFilter, (nounId, startTime, endTime) =>
+  //     processAuctionCreated(nounId, startTime, endTime),
+  //   );
+  //   nounsAuctionHouseContract.on(extendedFilter, (nounId, endTime) =>
+  //     processAuctionExtended(nounId, endTime),
+  //   );
+  //   nounsAuctionHouseContract.on(settledFilter, (nounId, winner, amount) =>
+  //     processAuctionSettled(nounId, winner, amount),
+  //   );
+  // };
+
+  // loadState();
 
   return <></>;
 };
